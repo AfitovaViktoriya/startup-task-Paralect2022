8a6110f79a639612d921fcd0d40bb8af197900ec
AfitovaViktoriya <afitovaviktoria@gmail.com>
2022-05-18 18:49:53 +0300

    feat: added searchbar and assets

--- github-api/src/App.js
+++ github-api/src/App.js
@@ -1,8 +1,13 @@
+import React, { useState } from 'react';
 import './App.css';
+import { SearchBar } from './components/SearchBar/SearchBar';
 
 function App() {
+  const [userName, setUserName] = useState('');
+
   return (
     <div className="App">
+      <SearchBar setUserName={setUserName} />
     </div>
   );
 }
